a7d9abe206d810ce88aabddcc305435f8f82cf03
Cyrodii <cyrodii@cyrodb.net>
2019-04-17 07:05:34 -0400

    f again

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,8 +19,8 @@ const axios = require('axios');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('bot-ui', require('./components/BotUi.vue').default);
