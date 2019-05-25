a0ae7cae6c5ad56db390c9fd3a2a1606ed92e1d9
Cyrodii <cyrodii@cyrodb.net>
2019-05-25 06:19:23 -0400

    live debugging test meme

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
 			<div class="row">
 					<progress-bar v-bind:type="'user'"></progress-bar>
 					<totals-widget v-bind:type="'tips'"></totals-widget>
-					<totals-widget v-bind:type="'paycheques'"></totals-widget>
+					{{-- <totals-widget v-bind:type="'paycheques'"></totals-widget> --}}
 			</div>
 		</div>
 		<div class="row row-deck">
